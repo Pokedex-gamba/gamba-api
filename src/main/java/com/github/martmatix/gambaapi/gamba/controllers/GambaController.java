04ea5957b65e5c66c167abefe5626e44d0460fe6
MartmatiX <martin.malir@gmail.com>
2024-10-16 18:46:25 +0200

    Basic extraction of public key

--- src/main/java/com/github/martmatix/gambaapi/gamba/controllers/GambaController.java
+++ src/main/java/com/github/martmatix/gambaapi/gamba/controllers/GambaController.java
@@ -1,7 +1,10 @@
 package com.github.martmatix.gambaapi.gamba.controllers;
 
 import com.github.martmatix.gambaapi.gamba.pokemon.Pokemon;
+import com.github.martmatix.gambaapi.services.KeyLoaderService;
 import com.github.martmatix.gambaapi.services.PokemonService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,16 +15,35 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.security.PublicKey;
 import java.util.List;
 
 @RestController
 public class GambaController {
 
     private PokemonService pokemonService;
+    private KeyLoaderService keyLoaderService;
 
     @GetMapping(path = "/pokemon/gamba/getRandomPokemon")
     public ResponseEntity<?> getRandomPokemon(@RequestHeader("Authorization") String authHeader) {
         try {
+            String token = authHeader.replace("Bearer", "").trim();
+
+            PublicKey publicKey = keyLoaderService.getPublicKey("""
+                    -----BEGIN PUBLIC KEY-----
+                    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvLpTEski8k6IpKntmaXA4Pg4LfW1kmYaK5Y+UctOoaKssyUIE+nZ2dEx4KtAIqsiERsKGUOfxJdxDGS0urmkxVXRnhbyOdAfflOMGEcE8sQYnNHillyvUtMACJw9/GzS7v8k+i18lPXv16b3oUXEt+sBoRSuJYGxE16sxVNdyyFECle6wbobL9Py6sIEYuIpwH0ozumRR+LSrJGjnWR2JDziGBvOmL+4x/RYHplCHzcdCvBjLZS4hMSSfYY3nCtfpWEvfIbr604aA0WT9ZfHJSnXpE9Cy77BYVkRYcb32zjnkXJ7yUJpXjVgdI/gtUi60pjYt+FhNq/SDe+xH11/0QIDAQAB
+                    -----END PUBLIC KEY-----`
+                    """);
+
+            Claims claims = Jwts.parser()
+                    .verifyWith(publicKey)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            String userId = claims.get("user_id", String.class);
+            System.out.println(userId);
+
             Flux<Pokemon> pokemonFlux = pokemonService.getPokemon(authHeader);
             Mono<List<Pokemon>> pokemonMono = pokemonFlux.collectList();
 
@@ -38,6 +60,11 @@ public class GambaController {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body("{\"error\": \"Internal Server Error: " + e.getMessage() + "\"}");
         }
+    }
+
+    @Autowired
+    public void setKeyLoaderService(KeyLoaderService keyLoaderService) {
+        this.keyLoaderService = keyLoaderService;
     }
 
     @Autowired

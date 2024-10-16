6d612564036a47b9b138c5a1f68a1c3dd313ed2e
MartmatiX <martin.malir@gmail.com>
2024-10-16 16:27:26 +0200

    First commit

---
+++ src/main/java/com/github/martmatix/gambaapi/GambaApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.martmatix.gambaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GambaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GambaApiApplication.class, args);
+    }
+
+}

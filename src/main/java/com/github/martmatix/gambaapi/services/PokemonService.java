cd5bd8d41fd03b0ff49fa5ad76676f4be40e966a
MartmatiX <martin.malir@gmail.com>
2024-10-17 22:05:10 +0200

    Docker fix and local url resolve

--- src/main/java/com/github/martmatix/gambaapi/services/PokemonService.java
+++ src/main/java/com/github/martmatix/gambaapi/services/PokemonService.java
@@ -2,6 +2,7 @@ package com.github.martmatix.gambaapi.services;
 
 import com.github.martmatix.gambaapi.gamba.pokemon.Pokemon;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
@@ -11,8 +12,11 @@ public class PokemonService {
 
     private WebClient.Builder builder;
 
+    @Value("${pokemon.api.url}")
+    private String pokemonHost;
+
     public Flux<Pokemon> getPokemon(String authHeader) {
-        WebClient webClient = builder.baseUrl("http://localhost:8888").build();
+        WebClient webClient = builder.baseUrl(pokemonHost).build();
 
         return webClient.get()
                 .uri("/pokemon/get_random/1")

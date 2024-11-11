6fc0a08902eb87b393eae1a06f33b2f1dfe12c09
MartmatiX <martin.malir@gmail.com>
2024-11-11 21:02:12 +0100

    Endpoint grants

--- src/main/java/com/github/martmatix/gambaapi/gamba/controllers/GambaController.java
+++ src/main/java/com/github/martmatix/gambaapi/gamba/controllers/GambaController.java
@@ -9,10 +9,8 @@ import com.github.martmatix.gambaapi.services.PokemonService;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +18,6 @@ import org.springframework.web.reactive.function.client.WebClientResponseExcepti
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import javax.sql.DataSource;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -37,8 +34,6 @@ public class GambaController {
     private PokemonService pokemonService;
     private KeyLoaderService keyLoaderService;
     private GambaService gambaService;
-
-    private DataSource dataSource;
 
     @GetMapping(path = "/pokemon/gamba/getRandomPokemon")
     public ResponseEntity<?> getRandomPokemon(@RequestHeader("Authorization") String authHeader) {
@@ -86,27 +81,6 @@ public class GambaController {
 
             List<GambaEntity> gambaByUserId = gambaService.getGambaByUserId(userId);
             return ResponseEntity.ok(gambaByUserId);
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Internal Server Error: " + e.getMessage() + "\"}");
-        }
-    }
-
-    @GetMapping(path = "/pokemon/gamba/clearDb")
-    public ResponseEntity<?> clearDb(@RequestHeader("Authorization") String authHeader) {
-        try {
-            String userId = getUserIdFromToken(authHeader);
-            if (userId.equals(ErrorCodes.TOKEN_EXTRACTION_ERROR.getCode())) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Unable To Process Request: " + ErrorCodes.TOKEN_EXTRACTION_ERROR.getCode() + "\"}");
-            }
-            if (userId.equals(ErrorCodes.PUBLIC_NOT_FOUND.getCode())) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Unable To Process Request: " + ErrorCodes.PUBLIC_NOT_FOUND.getCode() + "\"}");
-            }
-
-            ClassPathResource classPathResource = new ClassPathResource("initDatabase.sql");
-            ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator(classPathResource);
-            databasePopulator.execute(dataSource);
-
-            return ResponseEntity.ok("{\"ok\": \"Database Cleared And Initialized Successfully\"}");
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("{\"error\": \"Internal Server Error: " + e.getMessage() + "\"}");
         }
@@ -190,8 +164,4 @@ public class GambaController {
         this.gambaService = gambaService;
     }
 
-    @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 }
